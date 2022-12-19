0e4506397ff682b22837e93be4a21f1890579eed
Sungjae Park <nicesj@nicesj.com>
2022-12-19 05:41:25 +0000

    Implement the simplest wayland-server code
    [Problem] Get started coding!
    [Solution] Create a display object and socket for the server initialization
    
    Signed-off-by: Sungjae Park <nicesj@nicesj.com>

--- src/main.cc
+++ src/main.cc
@@ -1,7 +1,28 @@
+#include <wayland-server.h>
+
+#include <cstring>
+#include <cstdlib>
 #include <cstdio>
+#include <cerrno>
 
 int main(int argc, char *argv[])
 {
-	printf("Main\n");
+	wl_display *display = wl_display_create();
+	if (!display) {
+		fprintf(stderr, "Unable to create the wayland display\n");
+		return -EFAULT;
+	}
+
+	const char *socket = wl_display_add_socket_auto(display);
+	if (!socket) {
+		fprintf(stderr, "Unable to add socket to the wayland display\n");
+		wl_display_destroy(display);
+		return -EFAULT;
+	}
+
+	printf("Running the wayland display on %s\n", socket);
+	wl_display_run(display);
+
+	wl_display_destroy(display);
 	return 0;
 }
