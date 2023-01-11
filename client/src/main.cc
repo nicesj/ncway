2d02766934b86f036893a7ae2a2735d3208eb4f6
Sung-jae Park <nicesj@nicesj.com>
2023-01-11 08:31:25 +0000

    Add eventloop for the client
    [Problem] Client needs to run the event-loop
    [Solution] Adds the simplest event loop using the epoll()
    
    Signed-off-by: Sung-jae Park <nicesj@nicesj.com>

--- client/src/main.cc
+++ client/src/main.cc
@@ -2,7 +2,14 @@
 
 #include <cstdio>
 #include <cerrno>
+#include <cstring>
+
 #include <wayland-client.h>
+#include <sys/epoll.h>
+#include <unistd.h>
+
+#define MAX_EVENTS 10
+#define TIMEOUT_IN_MS -1
 
 static void registry_handle_global(void *data, wl_registry *registry, uint32_t name, const char *interface, uint32_t version)
 {
@@ -37,6 +44,70 @@ int main(int argc, char *argv[])
 	wl_registry *registry = wl_display_get_registry(display.get());
 	wl_registry_add_listener(registry, &registry_listener, nullptr);
 	wl_display_roundtrip(display.get());
+
+	epoll_event events[MAX_EVENTS + 1];
+
+	int handle = epoll_create1(EPOLL_CLOEXEC);
+	if (handle > 0) {
+		epoll_event ev;
+		ev.events = EPOLLIN | EPOLLPRI | EPOLLERR | EPOLLOUT;
+		ev.data.ptr = reinterpret_cast<void *>(0x01);
+
+		int wl_fd = wl_display_get_fd(display.get());
+		if (wl_fd < 0) {
+			if (close(handle) < 0) {
+				fprintf(stderr, "close: %s\n", strerror(errno));
+			}
+			return 0;
+		}
+
+		if (epoll_ctl(handle, EPOLL_CTL_ADD, wl_fd, &ev) < 0) {
+			fprintf(stderr, "epoll_ctl: %s\n", strerror(errno));
+			if (close(handle) < 0) {
+				fprintf(stderr, "close: %s\n", strerror(errno));
+			}
+			return 0;
+		}
+
+		do {
+			int ret = epoll_pwait(handle, events, MAX_EVENTS, TIMEOUT_IN_MS, nullptr);
+			if (ret < 0) {
+				if (errno == EINTR) {
+					printf("Interrupted\n");
+					continue;
+				}
+
+				fprintf(stderr, "Something goes wrong: %s\n", strerror(errno));
+				break;
+			} else if (ret == 0) {
+				printf("Timed out\n");
+				continue;
+			}
+
+			for (int i = 0; i < ret; ++i) {
+				if (events[i].data.ptr == nullptr) {
+					printf("Interrupted\n");
+					continue;
+				}
+
+				if (events[i].events & (EPOLLIN | EPOLLPRI)) {
+					// Read
+					if (wl_display_dispatch(display.get()) < 0) {
+						fprintf(stderr, "dispatch: %s\n", strerror(errno));
+					}
+				}
+
+				if (events[i].events & EPOLLOUT) {
+					// Write
+					// wl_display_flush(display.get())
+				}
+
+				if (events[i].events & EPOLLERR) {
+					// Error
+				}
+			}
+		} while (true);
+	}
 
 	printf("Connection established\n");
 	return 0;
